b4c9f1d735263b067b184455cd61519562a2fe27
sergiomaia <sergiomaia@gmail.com>
2016-11-07 00:43:39 -0200

    creating accounts resources

---
+++ spec/factories/accounts.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :account do
+    pitch "MyText"
+    subdomain "MyString"
+    owner_id 1
+  end
+end
--- spec/features/account_create_feature_spec.rb
+++ spec/features/account_create_feature_spec.rb
@@ -5,12 +5,12 @@ describe 'account create' do
 		visit root_path
 		click_link 'Create Account'
 
-		fill_in 'Name',      with: 'John Doe'
-		fill_in 'Email',     with: 'johndoe@johndoe.com'
+		#fill_in 'Name',      with: 'John Doe'
+		#fill_in 'Email',     with: 'johndoe@johndoe.com'
 		fill_in 'Pitch',     with: 'The best school on line ever'
 		fill_in 'Subdomain', with: 'test_subdomain'
-		fill_in 'Password',  with: 'pw'
-		fill_in 'Password Confirmation', with: 'pw'
+		#fill_in 'Password',  with: 'pw'
+		#fill_in 'Password Confirmation', with: 'pw'
 		click_button 'Create Account'
 
 		expect(page).to have_content('Signed up successfully')
